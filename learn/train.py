Fixed Trainer. Network is able to learn to average three numbers from a continuous range between 1 and 10. I also added the save and reload functions. Models are saved in the tmp directory. Because the network is estimating values to some decimal precision, I gave Trainer a default delta value. Later this should be changed so that Trainer can be reused for estimating other functions instead of just doing regression.

learn/train.py
@@ -12,18 +12,22 @@
 import tensorflow as tf
 from . import datahandler as dh
 from . import mlp
+import os
+import numpy as np
 
 class Trainer:
-  def __init__(self, feature_iters, output_size, label, data_size=10000,
-               max_cost=-1, learning_rate=0.001, training_epochs=1000,
-               batch_size=100, display_step=1):
+  def __init__(self, feature_iters, output_size, label, data_size=500000,
+               max_cost=-1, learning_rate=0.01, training_epochs=20,
+               batch_size=100, display_step=1, delta=0.0035, save_interval=10):
     # training parameters
-    self.data_size        = data_size
+    self.data_size       = data_size
     self.max_cost        = max_cost
     self.learning_rate   = learning_rate
     self.training_epochs = training_epochs
     self.batch_size      = batch_size
     self.display_step    = display_step
+    self.delta           = delta
+    self.save_interval   = save_interval
 
     self.dataset = dh.DataHandler(
             feature_iters=feature_iters,
@@ -31,21 +35,21 @@ class Trainer:
             label=label,
             batch_size=batch_size)
 
-    # Network hyperparameters
+    # Network
     self.input_size  = self.dataset.get_input_size()
     self.output_size = self.dataset.get_output_size()
-
-    # tf Graph input
-    self.x = tf.placeholder("float", [None, self.input_size])
-    self.y = tf.placeholder("float", [None, self.output_size])
-
-    self.neural_net = mlp.MultilayerPerceptron(self.x, self.input_size, self.output_size).getNetwork()
-    self.cost      = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.neural_net, self.y))
-    self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.cost)
-    self.init      = tf.initialize_all_variables()
+    self.x           = tf.placeholder("float", [None, self.input_size])
+    self.y           = tf.placeholder("float", [None, self.output_size])
+    self.neural_net  = mlp.MultilayerPerceptron(self.x, self.input_size, self.output_size).getNetwork()
+    self.cost        = tf.nn.l2_loss(self.neural_net - self.y)
+    self.optimizer   = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.cost)
+    self.init        = tf.initialize_all_variables()
+    self.saver       = tf.train.Saver()
 
   def train(self):
     with tf.Session() as sess:
+        if os.path.exists(os.getcwd()+"/tmp/model.ckpt"):
+          self.saver.restore(sess, "./tmp/model.ckpt")
         sess.run(self.init)
 
         # train
@@ -55,6 +59,7 @@ class Trainer:
 
             for i in range(total_batch):
               batch_x, batch_y = self.dataset.get_batch()
+
               # backprop
               _, c = sess.run([self.optimizer, self.cost], feed_dict={self.x: batch_x, self.y: batch_y})
               avg_cost += c / total_batch
@@ -64,15 +69,21 @@ class Trainer:
                         "{:.9f}".format(avg_cost))
             if avg_cost < self.max_cost:
                 break
+            if (epoch+1) % self.save_interval == 0:
+              save_path = self.saver.save(sess, "./tmp/model.ckpt")
+              print("network saved in %s" % save_path)
         print("optimized...")
 
-        # test model
-        correct_prediction = tf.equal(tf.argmax(self.neural_net, 1), tf.argmax(self.y, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-        test_x, test_y = self.dataset.get_batch()
-        print(test_x)
-        print(test_y)
-        print("accuracy:", accuracy.eval({self.x: test_x, self.y: test_y}))
+  def test(self):
+    with tf.Session() as sess:
+      if os.path.exists(os.getcwd()+"/tmp/model.ckpt"):
+          self.saver.restore(sess, "./tmp/model.ckpt")
+
+      accuracy = tf.reduce_mean(tf.cast(np.absolute(self.neural_net - self.y) < self.delta, tf.float32))
+      test_x, test_y = self.dataset.get_batch()
+      print('nn_y:\n',sess.run(self.neural_net, feed_dict={self.x: test_x}))
+      print('test_y:\n',test_y)
+      print("accuracy:", sess.run(accuracy, feed_dict={self.x: test_x, self.y: test_y}))
 
   def getNN(self):
     return self.neural_net